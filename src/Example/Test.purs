515951064f3fa6136265e7643dee63479b6b745e
Hugo Josefson <hugo@josefson.org>
2016-10-29 11:59:48 +0200

    isEmptyString

--- src/Example/Test.purs
+++ src/Example/Test.purs
@@ -3,4 +3,8 @@ module Example.Test (testing) where
 bar :: String
 bar = "c"
 
+isEmptyString :: String -> Boolean
+isEmptyString "" = true
+isEmptyString _ = false
+
 foreign import testing :: String
